95d474700880de73ab691f8422afc746e36589f1
PATHAKmridul17 <90190954+PATHAKmridul17@users.noreply.github.com>
2021-09-07 08:58:52 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+var readlinSync=require("readline-sync");
+console.log("Welcome to the marvel superheroes quiz");
+let username=readlinSync.question("What's your name: \n");
+console.log('\n')
+console.log(username+" How hard you are a marvel fan");
